ba181672967948e4f5935614f70f0c9dd4798e42
Rania Cht <raniachettab3@gmail.com>
2023-11-13 13:29:47 +0100

    feat: add login page

--- packages/components/ui/index.tsx
+++ packages/components/ui/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 
 // component exports
+export * from "./Login";
 export * from "./Button";
 export * from "./Header";
---
+++ packages/components/ui/stories/Login.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Login } from "..";
+
+export default {
+  title: "Login",
+  component: Login,
+};
+
+export const Default = () => <Login />;
