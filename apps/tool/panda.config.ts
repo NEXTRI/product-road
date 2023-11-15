d692e495e96d7603bd81bea342843545fce19d56
Rania Cht <raniachettab3@gmail.com>
2023-11-15 19:08:40 +0100

    feat: login page

---
+++ apps/tool/panda.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ["./app/components/**/*.tsx", "./app/**/*.tsx"],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {
+      tokens: {
+        colors: {
+          athensGray: { value: "#f9f9fb" },
+          frenchRose: { value: "#EF567C" },
+          nevada: { value: "#64676B" },
+          azureRadiance: { value: "#1675f0" },
+        },
+      },
+    },
+  },
+
+  // The output directory for your css system
+  outdir: "styled-system",
+});
