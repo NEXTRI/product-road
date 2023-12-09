2b960b15fd306afc70ec6b1b523514597f63eacd
ousszizou <odjaidri@gmail.com>
2023-12-09 17:14:54 +0100

    chore(api): implement database migrations

---
+++ api/db/migrations/000001_init_schema.down.sql
@@ -0,0 +1,12 @@
+ALTER TABLE IF EXISTS comments DROP CONSTRAINT IF EXISTS fk_comments_feedback_id;
+ALTER TABLE IF EXISTS comments DROP CONSTRAINT IF EXISTS fk_comments_user_id;
+ALTER TABLE IF EXISTS feedbacks DROP CONSTRAINT IF EXISTS fk_feedbacks_user_id;
+ALTER TABLE IF EXISTS feedbacks DROP CONSTRAINT IF EXISTS fk_feedbacks_external_user_id;
+ALTER TABLE IF EXISTS feedbacks DROP CONSTRAINT IF EXISTS fk_feedbacks_project_id;
+ALTER TABLE IF EXISTS projects DROP CONSTRAINT IF EXISTS fk_projects_user_id;
+
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS feedbacks;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS external_users;
+DROP TABLE IF EXISTS users;
