47c2f547a0ed83bd6ad8896ac63c2424817c7932
Mahi samia <mahisamia13@gmail.com>
2024-01-07 13:39:25 +0100

    feat: add root layout components
    basic version to start with

---
+++ frontend/components/layouts/index.js
@@ -0,0 +1,6 @@
+import Logo from "./Logo";
+import Navigation from "./Navigation";
+import Sidebar from "./Sidebar";
+import { ComboboxDemo } from "./ComboboxDemo";
+
+export { Logo, Navigation, Sidebar, ComboboxDemo };
