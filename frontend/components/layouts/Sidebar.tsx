1420a986fa0f38d0e261b7e72a32772e43831d5f
Mahi samia <mahisamia13@gmail.com>
2024-01-08 14:33:30 +0100

    styling: add toggle option to sidebar

--- frontend/components/layouts/Sidebar.tsx
+++ frontend/components/layouts/Sidebar.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Logo, Navigation, ComboboxDemo } from "./index";
+import { cn } from "@/lib/utils";
 
-const Sidebar = () => {
+const Sidebar = ({ sidebarOpen }) => {
+  const toggleStyle = sidebarOpen ? "-ml-[250px]" : "ml-0";
   return (
-    <aside className="bg-white py-8 min-w-60 flex flex-col justify-between">
-      <div className="">
+    <aside
+      className={cn(
+        "bg-white py-8 w-[250px] flex flex-col justify-between transition-all delay-200",
+        toggleStyle
+      )}
+    >
+      <div>
         <Logo />
         <ComboboxDemo />
         <Navigation />
       </div>
-
       <ComboboxDemo />
     </aside>
   );
--- frontend/components/layouts/index.js
+++ frontend/components/layouts/index.js
@@ -1,6 +1,7 @@
 import Logo from "./Logo";
 import Navigation from "./Navigation";
 import Sidebar from "./Sidebar";
+import ToggleSidebar from "./ToggleSidebar";
 import { ComboboxDemo } from "./ComboboxDemo";
 
-export { Logo, Navigation, Sidebar, ComboboxDemo };
+export { Logo, Navigation, Sidebar, ComboboxDemo, ToggleSidebar };
