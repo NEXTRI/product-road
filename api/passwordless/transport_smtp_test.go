e3c9bee7dfd4f0a03b2592b30a07e4f2d2d73551
ousszizou <odjaidri@gmail.com>
2024-01-08 09:22:12 +0100

    refactor(api): change test's email

--- api/passwordless/transport_smtp_test.go
+++ api/passwordless/transport_smtp_test.go
@@ -8,7 +8,7 @@ import (
 func TestSMTPTransport_SendToken_MagicLink_NewUser(t *testing.T) {
 	smtpTransport := NewSMTPTransport(config)
 
-	email := "odjaidri@gmail.com"
+	email := "test@gmail.com"
 	token := "qyGdH7Ouuhq8ONOUX2OUKWGB-One_K2Lh0k5F4WhaU8"
 
 	err := smtpTransport.SendToken(context.Background(), email, token, TokenTypeString, true)
@@ -20,7 +20,7 @@ func TestSMTPTransport_SendToken_MagicLink_NewUser(t *testing.T) {
 func TestSMTPTransport_SendToken_CodePIN_NewUser(t *testing.T) {
 	smtpTransport := NewSMTPTransport(config)
 
-	email := "odjaidri@gmail.com"
+	email := "test@gmail.com"
 	token := "123456"
 
 	err := smtpTransport.SendToken(context.Background(), email, token, TokenTypePin, true)
@@ -32,7 +32,7 @@ func TestSMTPTransport_SendToken_CodePIN_NewUser(t *testing.T) {
 func TestSMTPTransport_SendToken_MagicLink_ExistingUser(t *testing.T) {
 	smtpTransport := NewSMTPTransport(config)
 
-	email := "odjaidri@gmail.com"
+	email := "test@gmail.com"
 	token := "qyGdH7Ouuhq8ONOUX2OUKWGB-One_K2Lh0k5F4WhaU8"
 
 	err := smtpTransport.SendToken(context.Background(), email, token, TokenTypeString, false)
@@ -44,7 +44,7 @@ func TestSMTPTransport_SendToken_MagicLink_ExistingUser(t *testing.T) {
 func TestSMTPTransport_SendToken_CodePIN_ExistingUser(t *testing.T) {
 	smtpTransport := NewSMTPTransport(config)
 
-	email := "odjaidri@gmail.com"
+	email := "test@gmail.com"
 	token := "123456"
 
 	err := smtpTransport.SendToken(context.Background(), email, token, TokenTypePin, true)
