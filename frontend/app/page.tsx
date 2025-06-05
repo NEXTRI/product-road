6719a284ed5fa8ebe7d989040de2756989776b91
oussama djaidri <odjaidri@gmail.com>
2025-06-05 15:59:12 +0100

    chore: remove frontend directory and all its contents

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="content">
-      <h1>this is the content</h1>
-    </div>
-  );
-}
--- frontend/components/layouts/index.js
+++
@@ -1,7 +0,0 @@
-import Logo from "./Logo";
-import Navigation from "./Navigation";
-import Sidebar from "./Sidebar";
-import ToggleSidebar from "./ToggleSidebar";
-import { ComboboxDemo } from "./ComboboxDemo";
-
-export { Logo, Navigation, Sidebar, ComboboxDemo, ToggleSidebar };
